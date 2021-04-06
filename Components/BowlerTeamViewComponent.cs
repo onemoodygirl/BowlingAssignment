bf4e44090e1b383441dad0213ee53adfe9c8cec1
Mallory :) <30938583+onemoodygirl@users.noreply.github.com>
2021-04-06 00:32:35 -0600

    Update BowlerTeamViewComponent.cs

--- Components/BowlerTeamViewComponent.cs
+++ Components/BowlerTeamViewComponent.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using MalPalBowling.Models;
 using System.Linq;
 
-
+//the view components file
 namespace MalPalBowling.Components
 {
     public class BowlerTeamViewComponent : ViewComponent
